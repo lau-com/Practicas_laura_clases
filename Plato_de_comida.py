15b4ab59076c1ba50dd8fd245c0911c7e66a4b15
lau-com <catagarco@gmail.com>
2022-03-26 09:59:04 -0500

    update

---
+++ Plato_de_comida.py
@@ -0,0 +1,23 @@
+
+from pdb import post_mortem
+
+
+class plato_de_comida}
+    def __init__(Self,verduras, frutas, cereales, tuberculos, lacteos, proteina, frutos_secos, tamaño_del_plato,tipo_de_plato,postre , bebida, maridaje, menu_dietetico, salsas):
+        Self.verduras= verduras
+        Self.frutas= frutas 
+        Self.cereales= cereales
+        Self.tuberculos= tuberculos
+        Self.lacteos= lacteos
+        Self.proteina= proteina
+        Self.frutos_secos= frutos_secos
+        Self.tamaño_del_plato= tamaño_del_plato
+        Self.tipo_de_plato= tipo_de_plato
+        Self.postre= postre 
+        Self.bebida= bebida
+        Self.maridaje= maridaje
+        Self.menu_dietetico= menu_dietetico
+        Self.salsas= salsas
+
+
+Salmon_con_verduras (True,False,True,False,False,True,)
